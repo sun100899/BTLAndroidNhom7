3f3b7fce9db39e978d6cc2c02470f6875013344e
Thanh Xuan Vu <vuthanhxuan99@gmail.com>
2021-05-25 23:16:07 +0700

    luu dlieu voi shared preferences

--- app/src/main/java/com/example/btlandroidnhom7/MainActivity.java
+++ app/src/main/java/com/example/btlandroidnhom7/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.btlandroidnhom7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TABLE_ACCS = "tableAccs";
+    public static final String USERNAME = "userName";
+    public static Boolean flag = false;
+
+    EditText editUserName;
+    Button btnCreate;
+    TextView textView;
+
+    private String userName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editUserName = findViewById(R.id.hoTen);
+        btnCreate = findViewById(R.id.btnCreate);
+        textView = findViewById(R.id.tvXinChao);
+
+        loadUser();
+        updateDataUser();
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+    }
+
+
+    public void loadUser(){
+        SharedPreferences sharedPreferences = getSharedPreferences(TABLE_ACCS, MODE_PRIVATE);
+        userName = sharedPreferences.getString(USERNAME, "");
+        if(userName!= ""){
+            flag = true;
+        }
+    }
+
+    public void updateDataUser(){
+//        if(flag==true){
+//            textView.setText("Xin chào: "+userName);
+//        }
+//        else {
+//            textView.setText("chua co du lieu");
+//        }
+        textView.setText(userName);
+    }
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(TABLE_ACCS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(USERNAME, editUserName.getText().toString());
+
+        editor.apply();
+        Toast.makeText(this, "Đã tạo người dùng", Toast.LENGTH_LONG).show();
     }
 }
